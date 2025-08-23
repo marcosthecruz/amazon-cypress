9578603d0d00501c41edcdb1bb89b72d795eab27
marcosthecruz <marcosthecruz@gmail.com>
2025-08-23 14:07:00 -0300

    Ajustes de erros no mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
   e2e: {
     baseUrl: "https://www.amazon.com.br",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
